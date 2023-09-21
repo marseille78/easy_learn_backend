51e91e73fb9b4f7f97338b7133905e90ac698535
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-09-21 19:25:15 +0300

    deploy project

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+require("dotenv").config();
 
 const app = express();
 
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatLogger));
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
-    name: "test"
-  })
+    name: "test",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const app = require('./app');
-const mongoose = require('mongoose');
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const DB_HOST = "mongodb+srv://marseille78:s37HHjTJJNoqGsuC@cluster0.td8knft.mongodb.net/easy_learn_db?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 5050 } = process.env;
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(5050, () => console.log(`Server running...`));
+    app.listen(PORT, () => console.log(`Server running...`));
   })
   .catch((err) => {
     console.log(err.message);
